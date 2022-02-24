c125e6b59a9c99404dcf4bf7274990a3487447d9
Michael Eggers <mseggers@protonmail.ch>
2022-02-24 12:35:45 +0100

    parse texture data

--- src/tools/polysoup/polysoup.cpp
+++ src/tools/polysoup/polysoup.cpp
@@ -14,9 +14,18 @@ struct Vertex
 	double x, y, z;
 };
 
+struct TextureData
+{
+	std::string name;
+	double xOffset, yOffset;
+	double rotation;
+	double xScale, yScale;
+};
+
 struct Face
 {
 	std::vector<Vertex> vertices;
+	TextureData texData;
 };
 
 enum TokenType
@@ -114,17 +123,22 @@ std::string getString(char* c, int* pos)
 	return result;
 }
 
+double parseNumber(char** c, int* pos)
+{
+	std::string number = "";
+	while (**c == '-' || **c >= '0' && **c <= '9' || **c == '.') {
+		number += **c; *c += 1; *pos += 1;
+	}
+	return atof(number.c_str());
+}
+
 Vertex getVertex(char* c, int* pos)
 {
 	Vertex v = { };
 	std::vector<double> values;
-	for (int i = 0; i < 3; i++) { // A face is defined of 3 vertices.
+	for (int i = 0; i < 3; i++) { // A face is defined by 3 vertices.
 		advanceToNextNonWhitespace(&c, pos);
-		std::string number = "";
-		while (*c == '-' || *c >= '0' && *c <= '9' || *c == '.') {
-			number += *c; c++; *pos += 1;
-		}
-		double value = atof(number.c_str());
+		double value = parseNumber(&c, pos);
 		values.push_back(value);
 	}
 
@@ -133,6 +147,11 @@ Vertex getVertex(char* c, int* pos)
 	v.z = values[2];
 
 	return v;
+}
+
+double getNumber(char* c, int* pos)
+{
+	return parseNumber(&c, pos);
 }
 
 std::string getTextureName(char* c, int* pos)
@@ -182,6 +201,8 @@ int main(int argc, char** argv)
 			std::string value = getString(&mapData[pos], &pos);
 		}
 		else if (token == LPAREN) { // Face
+			Face face = {};
+
 			Vertex v0 = getVertex(&mapData[pos], &pos);
 			token = getToken(&mapData[pos], &pos);
 
@@ -193,10 +214,23 @@ int main(int argc, char** argv)
 			Vertex v2 = getVertex(&mapData[pos], &pos);
 			token = getToken(&mapData[pos], &pos);
 
+			TextureData tex = {};
 			token = getToken(&mapData[pos], &pos);
-			std::string textureName = getTextureName(&mapData[pos], &pos);
-			token = getToken(&mapData[pos], &pos);
-
+			tex.name = getTextureName(&mapData[pos], &pos);
+			token = getToken(&mapData[pos], &pos); // Number
+			
+			tex.xOffset = getNumber(&mapData[pos], &pos);
+			token = getToken(&mapData[pos], &pos); // Number
+			tex.yOffset = getNumber(&mapData[pos], &pos);
+			token = getToken(&mapData[pos], &pos); // Number
+			tex.rotation = getNumber(&mapData[pos], &pos);
+			token = getToken(&mapData[pos], &pos); // Number
+			tex.xScale = getNumber(&mapData[pos], &pos);
+			token = getToken(&mapData[pos], &pos); // Number
+			tex.yScale = getNumber(&mapData[pos], &pos);
+			
+			face.vertices = std::vector<Vertex>{ v0, v1, v2 };
+			face.texData = tex;
 		}
 		
 		pos++;
